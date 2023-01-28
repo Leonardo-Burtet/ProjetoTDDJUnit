f7cd9e20319408b626269f76519e6e9fb0958344
Leonardo Burtet <leonardoburtet@outlook.com>
2023-01-28 19:48:32 -0300

    Projeto simples para estudo de TDD com JUnit

---
+++ tdd/src/main/java/br/com/alura/tdd/service/BonusService.java
@@ -0,0 +1,19 @@
+package br.com.alura.tdd.service;
+
+import java.math.BigDecimal;
+
+import br.com.alura.tdd.modelo.Funcionario;
+
+public class BonusService {
+
+	public BigDecimal calcularBonus(Funcionario funcionario) {
+		BigDecimal valor = funcionario.getSalario().multiply(new BigDecimal("0.1"));
+		
+		if (valor.compareTo(new BigDecimal("1000")) > 0) {
+			throw new IllegalArgumentException("Funcionario com salario marior do que R$10000 não pode receber bonus");
+		}
+		
+		return valor;
+	}
+
+}
